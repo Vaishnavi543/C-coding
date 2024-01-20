382a925dd4ff83024faa7539b9cc6d4140d70f1d
Vaishnavi Prajapati <vaishnavipeb23@gmail.com>
2024-01-20 15:15:10 +0530

    20january

--- p76/main.c
+++ p76/main.c
@@ -19,19 +19,24 @@ int main()
 	{
 		count++;
 	}
-	*ptr=(char*) malloc (count * (sizeof(char*)));
+	printf("number of strings in a file:%d",count);
+	fflush(stdout);
+	ptr=(char*) malloc (count * (sizeof(char*)));
 	fclose(fp);
 	fp=fopen("main.txt","r");
 	{
-	while(fgets(p,50,fp)!=NULL)
+		while(fgets(ptr[i],50,fp)!=NULL)
 		//for(i=0;i<count;i++)
 			{
 				ptr[i]=(char*) malloc (100*(sizeof(char)));
 				scanf("%s",ptr[i]);
 				fflush(stdout);
+				printf("%s\n",ptr[i]);
+				fflush(stdout);
 				i++;
 			}
 	}
+
 	for(i=0;i<count;i++)
 		{
 			printf("entered string %d:%s\n",i+1,ptr[i]);
